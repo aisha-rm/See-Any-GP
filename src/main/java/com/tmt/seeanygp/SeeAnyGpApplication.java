48b6c4f1868bf0e8662e4b9dec12ed1d2ad477c4
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2023-03-05 02:37:33 +0000

    first commit

---
+++ src/main/java/com/tmt/seeanygp/SeeAnyGpApplication.java
@@ -0,0 +1,13 @@
+package com.tmt.seeanygp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeAnyGpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeAnyGpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmt/seeanygp/SeeAnyGpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmt.seeanygp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeAnyGpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
